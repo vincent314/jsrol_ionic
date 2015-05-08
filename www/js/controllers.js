deee1cc707d4563935cfba5317d8352c11e21339
vincent <vincent314@hotmail.fr>
2015-05-08 23:31:10 +0200

    feat(events-tracks): introduce events and tracks tabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,20 @@
 angular.module('starter.controllers', [])
+    .controller('EventsCtrl', function ($scope) {
+    })
 
-.controller('DashCtrl', function($scope) {})
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        }
+    })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
